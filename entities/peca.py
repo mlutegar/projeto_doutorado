0b5fc88110399090d8b89b2d3e82dbe5daf9394b
mlutegar <mlutegar@gmail.com>
2024-07-02 12:15:33 -0300

    fix: consertando a classe peca

--- entities/peca.py
+++ entities/peca.py
@@ -1,3 +1,30 @@
+def definir_linha(pos_x: int) -> int:
+    """
+    Define a linha da peça com base na posição x.
+    """
+    linhas = [74, 143, 211, 284, 351, 424, 497, 565, 638, 706, 779, 846, 919]
+    margem_erro = 20
+
+    for i, linha in enumerate(linhas, start=1):
+        if abs(pos_x - linha) <= margem_erro:
+            return i
+    raise ValueError("Posição x fora do intervalo permitido.")
+
+
+def definir_coluna(pos_y: int) -> int:
+    """
+    Define a coluna da peça com base na posição y.
+    """
+    colunas = [86, 123, 163, 200, 239, 276, 316, 353, 393, 430, 470, 507, 547, 584, 624, 661, 701, 738, 778, 815,
+               855, 892, 932, 969, 1009, 1046, 1086, 1123, 1163, 1200, 1240, 1277, 1317, 1354]
+    margem_erro = 20
+
+    for i, coluna in enumerate(colunas, start=1):
+        if abs(pos_y - coluna) <= margem_erro:
+            return i
+    raise ValueError("Posição y fora do intervalo permitido.")
+
+
 class Peca:
     def __init__(self):
         self.uid = None
@@ -32,16 +59,16 @@ class Peca:
         """
         Atualiza a posição antiga da peça.
         """
-        self.linha_antiga = pos_x
-        self.coluna_antiga = pos_y
+        self.linha_antiga = definir_linha(pos_x)
+        self.coluna_antiga = definir_coluna(pos_y)
         self.posicao_antiga = [self.linha_antiga, self.coluna_antiga]
 
     def set_posicao_atual(self, pos_x, pos_y) -> None:
         """
         Atualiza a posição atual da peça.
         """
-        self.linha_atual = pos_x
-        self.coluna_atual = pos_y
+        self.linha_atual = definir_linha(pos_x)
+        self.coluna_atual = definir_coluna(pos_y)
         self.posicao_atual = [self.linha_atual, self.coluna_atual]
 
     def set_grupo(self, grupo: int) -> None:
