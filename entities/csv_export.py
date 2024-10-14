1a554111e44b6f00bb12bfcc45b332c7d5bffb7e
mlutegar <mlutegar@gmail.com>
2024-10-14 17:32:13 -0300

    feat: melhorando clusterizado

--- entities/csv_export.py
+++ entities/csv_export.py
@@ -228,10 +228,10 @@ class CsvExport:
                 previous_jogada = jogada
 
             # Plotando e salvando o grafo
-            plt.figure(figsize=(50, 50))
+            plt.figure(figsize=(28, 20))
             pos = nx.spring_layout(G, seed=42, k=0.5)
             nx.draw(G, pos, with_labels=True, labels=nx.get_node_attributes(G, 'label'), node_size=100,
-                    node_color=color_map, font_size=2, font_weight='bold', edge_color='gray', arrows=True, width=0.2)
+                    node_color=color_map, font_size=4, font_weight='bold', edge_color='gray', arrows=True, width=0.4)
             plt.title(f"Grafo das Ações do Jogador: {jogador}")
 
             grafo_path = grafo_path_root / f"{self.nome}_{jogador}.png"
